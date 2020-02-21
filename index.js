c497bc68c7cf7c7cba5a78d4a65c2ad6e00a0911
hongky-1994 <hongkyhvnh@gmail.com>
2020-02-21 23:39:53 +0700

    Setup header chat

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ let friend= document.getElementsByClassName('friend-a');
 for (let index = 0; index < friend.length; index++) {
     const e = friend[index];
     e.addEventListener('click', function(){
-        console.log(this);
-        
         let activeChat = document.getElementsByClassName('active-chat')[0];
-        console.log(activeChat);
-        
         activeChat.classList.remove("active-chat");
         this.classList.add("active-chat");
+        let name = document.getElementsByClassName('current-friend-name')[0];
+        name.innerText = this.lastElementChild.innerText;
+        let img = document.getElementsByClassName('chat-img')[0];
+        img.src = this.firstElementChild.firstElementChild.src;
     });
 }
